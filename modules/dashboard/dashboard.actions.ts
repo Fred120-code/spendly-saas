57a6ac483302e3d29b19201be6768be7a6a39ba7
Fred120-code <fredayemtsa@gmail.com>
2026-07-08 22:27:00 +0100

    feat/add server action in dashboard

---
+++ modules/dashboard/dashboard.actions.ts
@@ -0,0 +1,8 @@
+import {
+  getMyLastTransactionsAction,
+  getMyTotalTransactionAmountAction,
+  getMyTotalTransactionCountAction,
+} from "@/modules/transactions/transaction.actions";
+
+
+import { getMyEndBudgetCountAction, getMyBudgetDistributionAction, getMyPieChartDataAction} from "@/modules/budgets/budget.actions"
