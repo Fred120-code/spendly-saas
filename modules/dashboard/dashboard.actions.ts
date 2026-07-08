c9dbf2abf0169fac62ee51672313a5beb96da230
Fred120-code <fredayemtsa@gmail.com>
2026-07-08 22:44:32 +0100

    fix: add dasboard action

--- modules/dashboard/dashboard.actions.ts
+++ modules/dashboard/dashboard.actions.ts
@@ -1,8 +1,34 @@
+"use server";
+
 import {
   getMyLastTransactionsAction,
   getMyTotalTransactionAmountAction,
   getMyTotalTransactionCountAction,
 } from "@/modules/transactions/transaction.actions";
 
+import {
+  getMyEndBudgetCountAction,
+  getMyBudgetDistributionAction,
+  getMyPieChartDataAction,
+} from "@/modules/budgets/budget.actions";
 
-import { getMyEndBudgetCountAction, getMyBudgetDistributionAction, getMyPieChartDataAction} from "@/modules/budgets/budget.actions"
+export async function getDashboardData() {
+  const [amount, count, endBuget, budgetdata, piedata, lastransactions] =
+    await Promise.all([
+      getMyTotalTransactionAmountAction(),
+      getMyTotalTransactionCountAction(),
+      getMyEndBudgetCountAction(),
+      getMyBudgetDistributionAction(),
+      getMyPieChartDataAction(),
+      getMyLastTransactionsAction(5),
+    ]);
+
+  return {
+    amount,
+    count,
+    endBuget,
+    budgetdata,
+    piedata,
+    lastransactions,
+  };
+}
